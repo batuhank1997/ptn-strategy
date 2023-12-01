801c6db7aea362dece04c54f58919a97de4b2709
Batuhan <batuhank1997@hotmail.com>
2023-12-01 11:29:44 +0300

    info menu grid layout

--- Assets/_Dev/Game/Scripts/UI/Views/ProductionProduct.cs
+++ Assets/_Dev/Game/Scripts/UI/Views/ProductionProduct.cs
@@ -6,6 +6,8 @@ namespace _Dev.Game.Scripts.UI.Views
 {
     public class ProductionProduct : MonoBehaviour
     {
+        [SerializeField] private Button m_button;
+        
         public Image Icon;
         public TextMeshProUGUI Name;
 
@@ -13,6 +15,12 @@ namespace _Dev.Game.Scripts.UI.Views
         {
             Icon.sprite = icon;
             Name.text = name;
+            m_button.onClick.AddListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+            Debug.Log("Button clicked");
         }
     }
 }
