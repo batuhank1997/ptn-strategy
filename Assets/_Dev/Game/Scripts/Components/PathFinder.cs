e289153e4a6b984ffec2773b212935cd64df767c
Batuhan <batuhank1997@hotmail.com>
2023-12-03 12:19:42 +0300

    added find closest unoccupied cell if cell occupied

--- Assets/_Dev/Game/Scripts/Components/PathFinder.cs
+++ Assets/_Dev/Game/Scripts/Components/PathFinder.cs
@@ -9,6 +9,7 @@ namespace _Dev.Game.Scripts.Components
     public static class PathFinder
     {
         private static Dictionary<Vector2, Cell> _cells;
+        private const int UNI_COST = 1;
 
         public static List<Cell> FindPath(Vector2 startPosition, Vector2 targetPosition)
         {
@@ -24,6 +25,12 @@ namespace _Dev.Game.Scripts.Components
             gScore[startPosition] = 0;
             fScore[startPosition] = Heuristic(startPosition, targetPosition);
 
+            if (_cells.ContainsKey(targetPosition) && _cells[targetPosition].IsOccupied)
+            {
+                targetPosition = FindClosestUnoccupiedCell(targetPosition, startPosition).First(cell => !cell.IsOccupied).GetCoordinates();
+                Debug.Log($"[PATHFINDER] Target cell is occupied, closest unoccupied cell is {targetPosition}");
+            }
+
             while (openSet.Count > 0)
             {
                 var current = GetLowestFScore(openSet, fScore);
@@ -36,21 +43,19 @@ namespace _Dev.Game.Scripts.Components
 
                 foreach (var neighbor in GetNeighbors(current))
                 {
-                    if (!_cells.ContainsKey(neighbor))
-                        continue;
-                    
                     if (closedSet.Contains(neighbor))
                         continue;
 
-                    var tentativeGScore = gScore[current] + 1; // Assuming a uniform cost of 1 between tiles
+                    var tentativeGScore = gScore[current] + CalculateCost(current, neighbor);
 
                     if (openSet.Contains(neighbor) && !(tentativeGScore < gScore[neighbor])) continue;
-
+                    
                     cameFrom[neighbor] = current;
                     gScore[neighbor] = tentativeGScore;
                     fScore[neighbor] = gScore[neighbor] + Heuristic(neighbor, targetPosition);
 
-                    openSet.Add(neighbor);
+                    if (!openSet.Contains(neighbor))
+                        openSet.Add(neighbor);
                 }
             }
 
@@ -70,7 +75,7 @@ namespace _Dev.Game.Scripts.Components
             foreach (var tile in openSet)
             {
                 if (!fScore.ContainsKey(tile) || !(fScore[tile] < lowestFScore)) continue;
-
+                
                 lowestFScore = fScore[tile];
                 lowestFScoreTile = tile;
             }
@@ -93,7 +98,18 @@ namespace _Dev.Game.Scripts.Components
             return path;
         }
 
-        private static List<Vector2> GetNeighbors(Vector2 position)
+        private static float CalculateCost(Vector2 current, Vector2 neighbor)
+        {
+            var dx = Mathf.Abs(current.x - neighbor.x);
+            var dy = Mathf.Abs(current.y - neighbor.y);
+
+            if (dx > 0 && dy > 0)
+                return Mathf.Sqrt(2);
+            
+            return 1;
+        }
+
+        private static List<Vector2> GetNeighbors(Vector2 position, bool checkOccupied = true)
         {
             var neighbors = new List<Vector2>
             {
@@ -101,16 +117,61 @@ namespace _Dev.Game.Scripts.Components
                 new(position.x, position.y - 1),
                 new(position.x + 1, position.y),
                 new(position.x - 1, position.y),
-                new(position.x + 1, position.y + 1),
+                new(position.x + 1, position.y + 1), // Diagonal neighbors
                 new(position.x - 1, position.y - 1),
                 new(position.x + 1, position.y - 1),
                 new(position.x - 1, position.y + 1)
             };
 
-            neighbors = neighbors.Where(neighbor => _cells.ContainsKey(neighbor) && !_cells[neighbor].IsOccupied)
-                .ToList();
+            neighbors = checkOccupied ? 
+                neighbors.Where(neighbor => _cells.ContainsKey(neighbor) && !_cells[neighbor].IsOccupied).ToList() 
+                : neighbors.Where(neighbor => _cells.ContainsKey(neighbor)).ToList();
 
             return neighbors;
+        }
+
+        private static List<Cell> FindClosestUnoccupiedCell(Vector2 reversedStartPosition, Vector2 reversedTargetPosition)
+        {
+            _cells = GridManager.Instance.GetAllCells();
+            
+            var openSet = new HashSet<Vector2>();
+            var closedSet = new HashSet<Vector2>();
+            var cameFrom = new Dictionary<Vector2, Vector2>();
+            var gScore = new Dictionary<Vector2, float>();
+            var fScore = new Dictionary<Vector2, float>();
+            
+            openSet.Add(reversedStartPosition);
+            gScore[reversedStartPosition] = 0;
+            fScore[reversedStartPosition] = Heuristic(reversedStartPosition, reversedTargetPosition);
+
+            while (openSet.Count > 0)
+            {
+                var current = GetLowestFScore(openSet, fScore);
+                
+                if (current == reversedTargetPosition)
+                    return ReconstructPath(cameFrom, reversedTargetPosition);
+
+                openSet.Remove(current);
+                closedSet.Add(current);
+
+                foreach (var neighbor in GetNeighbors(current, false))
+                {
+                    if (closedSet.Contains(neighbor))
+                        continue;
+
+                    var tentativeGScore = gScore[current] + UNI_COST;
+
+                    if (openSet.Contains(neighbor) && !(tentativeGScore < gScore[neighbor])) continue;
+                    
+                    cameFrom[neighbor] = current;
+                    gScore[neighbor] = tentativeGScore;
+                    fScore[neighbor] = gScore[neighbor] + Heuristic(neighbor, reversedTargetPosition);
+
+                    openSet.Add(neighbor);
+                }
+            }
+
+            return null;
         }
     }
 }
